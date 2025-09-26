5ac2a3c08796d10ea1c41eea73a0a06788dd4464
srrod92 <rodrigogonzalez1992@hotmail.com>
2025-09-26 18:37:24 -0300

    Update clase7 listas enlazadas.

--- Fundamentos II Codigo/Clase 7/__main__.py
+++ Fundamentos II Codigo/Clase 7/__main__.py
@@ -7,6 +7,16 @@ lista = ListaEnlazada()
 
 lista.setear_head(head)
 
+lista.insertar_nodo(Nodo("Gollum", 10))
+
+lista.insertar_nodo(Nodo("Javier", 9000))
+lista.insertar_nodo(Nodo("Ian", 9000))
+lista.insertar_nodo(Nodo("Luz", 9000))
+lista.insertar_nodo(Nodo("Lucas", 9000))
+lista.insertar_nodo(Nodo("Sauron", 9999))
+lista.insertar_nodo(Nodo("Ramon", 10000))
+
+
 
 
 lista.eliminar_nodo(0)
